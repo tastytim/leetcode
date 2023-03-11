ce85c4410cef56f43ccdca3b2a3e27e3b23e9f18
tastytim <tastytimgm@gmail.com>
2023-03-11 14:46:11 +0100

    '

--- easy/missingnumber.js
+++ easy/missingnumber.js
@@ -9,11 +9,24 @@
  * @param {number[]} nums
  * @return {number}
  */
+var nums = [3, 0, 1];
 var missingNumber = function (nums) {
-    let arr = Array.from(Array(nums.length).keys());
-    let a = arr.filter((e) => !nums.includes(e));
-    if (a.length > 0) return a[0];
-    else return Math.max(...nums) + 1;
-  };
-  // @lc code=end
+  // let arr = Array.from(Array(nums.length).keys());
+  // let a = arr.filter((e) => !nums.includes(e));
+  // if (a.length > 0) return a[0];
+  // else return Math.max(...nums) + 1;
+
+
+
   
+  //formula for the sum of a sequence of consecutive numbers, which is:
+  //sum = (n * (n + 1)) / 2
+  const n = nums.length;
+  const expectedSum = (n * (n + 1)) / 2;
+  const actualSum = nums.reduce((sum, num) => sum + num, 0);
+
+  return expectedSum - actualSum;
+};
+
+console.log(missingNumber(nums));
+// @lc code=end
